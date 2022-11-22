fa90f854431da6caf24a30fec4a907d93f3ea19a
Егор Артемьев <egor@192.168.1.3>
2022-11-22 12:20:51 +0300

    Added Order and OrderItem models

--- payments_backend/payments/models.py
+++ payments_backend/payments/models.py
@@ -17,4 +17,31 @@ class Item(models.Model):
         return self.name
 
     def get_display_price(self):
-        return "{0:.2f}".format(self.price / 100)
+        return '{0:.2f}'.format(self.price / 100)
+
+
+class Order(models.Model):
+    class Meta:
+        verbose_name = _('order')
+        verbose_name_plural = _('orders')
+
+    description = models.TextField(_('description'))
+
+    def __str__(self):
+        return 'Order ({})'.format(self.id)
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, related_name='order_item', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.item.price * self.quantity
+
